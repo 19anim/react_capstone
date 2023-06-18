23bccb016f0a6a7bbe4ccdaa1721b1f847ede6a5
19.anim <nptatdt@gmail.com>
2023-06-19 01:23:40 +0700

    complete sign in component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,14 +1,15 @@
-import {
-  createAuthUserWithEmailAndPassword,
-  createUserDocRefFromAuth,
-} from "../../utils/firebase.utils";
-
 import { useState } from "react";
+
+import {
+  signInWithGooglePopup,
+  createUserDocRefFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase.utils";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
-import "./sign-up-form-styles.scss";
+import "./sign-in-form-styles.scss";
 
 const defaultFormField = {
   displayName: "",
@@ -17,7 +18,7 @@ const defaultFormField = {
   confirmPassword: "",
 };
 
-const SignUp = () => {
+const SignIn = () => {
   const [formField, setFormField] = useState(defaultFormField);
   const { displayName, email, password, confirmPassword } = formField;
 
@@ -25,26 +26,22 @@ const SignUp = () => {
     setFormField(defaultFormField);
   };
 
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocRefFromAuth(user);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (password !== confirmPassword) {
-      alert("Confirm password must be the same!");
-      return;
-    }
 
     try {
-      const { user } = await createAuthUserWithEmailAndPassword(
+      const response = await signInAuthUserWithEmailAndPassword(
         email,
         password
       );
-      await createUserDocRefFromAuth(user, { displayName });
-      resetFormField();
+      console.log(response);
     } catch (error) {
-      if (error.code === "auth/email-already-in-use") {
-        alert("Can not create user, the email is used");
-      } else {
-        console.log("Error in creating user", error.message);
-      }
+      console.log("Error in creating user", error.message);
     }
   };
 
@@ -53,20 +50,10 @@ const SignUp = () => {
     setFormField({ ...formField, [name]: value });
   };
   return (
-    <div className="sign-up-container">
-      <h2>Don't have an account?</h2>
-      <span>Sign up with your email and password</span>
+    <div className="sign-in-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <FormInput
-          label="Display Name"
-          inputOption={{
-            type: "text",
-            required: true,
-            name: "displayName",
-            onChange: handleChange,
-            value: displayName,
-          }}
-        />
         <FormInput
           label="Email"
           inputOption={{
@@ -87,21 +74,15 @@ const SignUp = () => {
             value: password,
           }}
         />
-        <FormInput
-          label="Confirm password"
-          inputOption={{
-            type: "password",
-            required: true,
-            name: "confirmPassword",
-            onChange: handleChange,
-            value: confirmPassword,
-          }}
-        />
-        <Button buttonType="google" type="submit">Sign up</Button>
-        
+        <div className="buttons-container">
+          <Button type="submit">Sign in</Button>
+          <Button buttonType="google" onClick={signInWithGoogle}>
+            Google sign in
+          </Button>
+        </div>
       </form>
     </div>
   );
 };
 
-export default SignUp;
+export default SignIn;
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,16 +1,12 @@
 import { signInWithGooglePopup, createUserDocRefFromAuth } from "../../utils/firebase.utils";
-import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignUp from "../../components/sign-up-form/sign-up-form.component";
+import SignIn from "../../components/sign-in-form/sign-in-form.component";
 
 const Authentication = () => {
-    const logInGoogle = async () => {
-        const { user } = await signInWithGooglePopup();
-        await createUserDocRefFromAuth(user);
-    }
     return (
         <div className="sign-in">
-            <h2>I am sign in page</h2>
-            <button onClick={logInGoogle}> Sign in with Google pop up</button>
-            <SignUpForm></SignUpForm>
+            <SignIn></SignIn>
+            <SignUp></SignUp>
         </div>
     )
 }
